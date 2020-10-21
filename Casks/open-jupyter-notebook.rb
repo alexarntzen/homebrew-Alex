64f32d06d7cfcaca85f1a7f3305fc3b9a2f045b9
Alexander Johan Arntzen <48533802+alexarntzen@users.noreply.github.com>
2020-10-22 00:23:30 +0200

    Update open-jupyter-notebook.rb

--- Casks/open-jupyter-notebook.rb
+++ Casks/open-jupyter-notebook.rb
@@ -1,5 +1,5 @@
 cask 'open-jupyter-notebook' do
-  version '0.2'
+  version '0.3'
   sha256 '4761f6e6fd05f75ead5e16cf4a24a4ef25bc7177df05313c79adfd700a1ee008'
 
   url "https://github.com/alexarntzen/open-jupyter-notebook/archive/master.zip"
