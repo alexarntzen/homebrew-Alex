62f6c42cdead928f7b7f00ff2a6b65fa4988f308
Alexander Johan Arntzen <48533802+alexarntzen@users.noreply.github.com>
2020-10-22 01:58:45 +0200

    Update open-jupyter-notebook.rb

--- Casks/open-jupyter-notebook.rb
+++ Casks/open-jupyter-notebook.rb
@@ -1,16 +1,16 @@
 cask 'open-jupyter-notebook' do
-  version '0.3'
+  version '0.4'
   sha256 '4d9f0aad57734dc88f8932cb41b388d7e96edb8fc31b00dfe257eedb6917c829'
 
-  url "https://github.com/alexarntzen/open-jupyter-notebook/archive/0.3.zip"
+  url "https://github.com/alexarntzen/open-jupyter-notebook/archive/0.4.tar.gz"
   name 'Open Jupyter Notebook'
   desc "App that opens Jypter Notebooks with double click"
   homepage 'https://github.com/alexarntzen/open-jupyter-notebook'
 
   installer script: {
-    executable: "open-jupyter-notebook-0.3/Open Jupyter Notebook.app/Contents/Resources/install_script.sh",
+    executable: "open-jupyter-notebook-0.4/install_script.sh",
   }
   
-  app 'open-jupyter-notebook-0.3/Open Jupyter Notebook.app'
+  app 'open-jupyter-notebook-0.4/Open Jupyter Notebook.app'
 
 end
