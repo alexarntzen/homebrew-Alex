b562882681ea2ef3612c52a50c822e1c98fa5ccb
Alexander Johan Arntzen <48533802+alexarntzen@users.noreply.github.com>
2020-10-21 23:52:28 +0200

    Update open-jupyter.rb

--- Casks/open-jupyter.rb
+++ Casks/open-jupyter.rb
@@ -1,10 +1,10 @@
-cask 'open-jupyter' do
+cask 'open-jupyter-notebook' do
   version '0.1'
   sha256 'a654918debff3d707fe9730b8f29198cd78c0365bb078a2f7f94c83551938363'
 
-  url "https://github.com/alexarntzen/openJupyterMacos/archive/master.zip"
-  name 'Open Jupyter'
-  homepage 'https://github.com/alexarntzen/openJupyterMacos'
+  url "https://github.com/alexarntzen/open-jupyter-notebook/archive/master.zip"
+  name 'Open Jupyter Notebook'
+  homepage 'https://github.com/alexarntzen/open-jupyter-notebook'
 
   app 'openJupyterMacos-master/JupyterNotebookOpen.app'
 end
